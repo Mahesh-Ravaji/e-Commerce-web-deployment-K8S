7d97bc88efbf0464bb3708e4c2959dc7e872fdd6
Mahesh-Ravaji <mahesh.wrvyu21@sinhgad.edu>
2024-07-06 10:52:27 +0530

    files updated lines added

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const addToCartButtons = document.querySelectorAll('button');
-    
-    addToCartButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        alert('Product added to cart!');
-      });
+  const addToCartButtons = document.querySelectorAll('button');
+  
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Product added to cart!');
     });
   });
-  
+});
